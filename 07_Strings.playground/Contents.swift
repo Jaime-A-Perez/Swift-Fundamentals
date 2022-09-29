4e8bdd12fa1e42ab6c8fe766d5fd879b6288533b
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-29 09:00:53 -0500

    Inicialitation and mutable

--- 07_Strings.playground/Contents.swift
+++ 07_Strings.playground/Contents.swift
@@ -1,5 +1,8 @@
 import UIKit
 
+
+//Strings
+
 let someString = "String cualquiera" // String literal (Una unica linea)
 
 let multiLIneString = """
@@ -9,5 +12,28 @@ let multiLIneString = """
 
 let wiseWords = "\"La imaginacion es mas grande que el saber\"- Alber Einsten"
 print(wiseWords)
+
+
+
+
+// Inicializacion y mutabilidad
+
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print("Noting")
+}else {
+    print("String con valor")
+}
+
+
+var newSomeString = "Un Caballo"
+newSomeString += " y un carruaje"
+
+//tipo de dato evaluado
+
+
+
 
 
