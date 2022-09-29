87690f0ce8a7ea31328f3e86238f6853e89f9620
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-29 09:58:11 -0500

    Arrays

---
+++ 08_Collections.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Array
+
+var someInts = [Int]()
+someInts.count
+someInts.append(31)
+someInts.count
+someInts = []
+
+
+var someDoubles = Array(repeating: 3.14, count: 3)
+someDoubles.count
+
+var moreDoubles = Array(repeating: 2.4, count: 4)
+
+var aLotOfDoubles = someDoubles + moreDoubles
+aLotOfDoubles.count
