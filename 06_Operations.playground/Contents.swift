bb54aea7f736f92c932fd479b2969613a5e4b8e5
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-29 07:56:24 -0500

    Aritmetic and asignation Operations

---
+++ 06_Operations.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+
+//Asignation
+let b = 10
+var a = 5
+a = b
+
+let (x,y) = (1,2)
+
+
+
+// Comparation
+
+if a == b {
+    print("los valores de a y b son iguales")
+}
+
+
+1+2
+5-2
+2*3
+10.4/2.4
+9/4 //cociente
+9%4 //restante
+
+"hello " + "World"
+
+let five = 5
+let minusFive = -five
+let plusFive = -minusFive
+
+var number = 5
+number += 3
+number -= 2
