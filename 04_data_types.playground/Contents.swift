86ab38843c1b8da62140553c9d65897a2b6c190a
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-28 20:31:39 -0500

    Booleans

---
+++ 04_data_types.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let oreangesAreOrange = true
+let foodIsDelicius = false
+
+var isAged : Bool
+
+isAged = false
+
+if isAged {
+    print("Puedes entrar a la fiesta")
+}else {
+    print("NO puedes entrar a la fiesta")
+}
+
+
+var age = 31
+if age >= 18 {
+    print("puedes entrar en la fiesta")
+}
+
