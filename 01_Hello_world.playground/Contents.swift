73f39251a3532349c890842ac3ac3e24e331f1db
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-28 19:38:14 -0500

    Consts, vars; Type Annotations; Nomenclature to name of vars; Comments

---
+++ 01_Hello_world.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// 02 - Consts and Variables
+
+let maximunNumberOfLoginAttempts = 3
+
+var currentLogginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+
+// 03 - Type Annotations
+
+
+var welcomeMessange : String
+
+welcomeMessange = "Hola, ¿que tal?"
+
+var red, green, blue : Double
+
+// 04 - Nomenclature to name of vars
+
+let π = 3.1416
+print(π)
+
+var 😇 = "Saint"
+print(😇)
+
+
+// Print values whit the function "print"
+
+print("El numero del login actual es: \(currentLogginAttempt) de un total disponible de: \(maximunNumberOfLoginAttempts)")
+
+// Comments
+
+// Comentario general
+
+/* Este es un comentario
+ multilinea */
+
+/*Comentario multilinea
+ /* Este es un comentario anidado */
+ */
+
+
