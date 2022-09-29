0dd1226ce301459686079510ea1fd2574a4cd696
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-28 19:53:05 -0500

    Integers

---
+++ 03_numbers.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+
+// Ints 8 bits
+let age : UInt8 = 31
+
+let minValuePositive8Bits = UInt8.min //2^0 - 1
+let maxValuePositive8Bits = UInt8.max //2^8 - 1
+
+let maxValue8Bits = Int8.max
+let minValue8Bits = Int8.min
+
+// Ints 16 bits
+let minValuePositive16Bits = UInt16.min //2^0 - 1
+let maxValuePositive16Bits = UInt16.max //2^16 - 1
+
+let maxValue16Bits = Int16.max
+let minValue16Bits = Int16.min
+
+// Ints 32 bits
+let minValuePositive32Bits = UInt32.min //2^0 - 1
+let maxValuePositive32Bits = UInt32.max //2^32 - 1
+
+let maxValue32Bits = Int32.max
+let minValue32Bits = Int32.min
+
+// Ints 64 bits
+let minValuePositive64Bits = UInt64.min //2^0 - 1
+let maxValuePositive64Bits = UInt64.max //2^64 - 1
+
+let maxValue64Bits = Int64.max
+let minValue64Bits = Int64.min
+
+
+
+
+
