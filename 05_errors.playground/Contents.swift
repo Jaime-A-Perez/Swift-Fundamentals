920b62963427171d07180591f22b86bf7cd59e0e
Jaime A Perez R <catecppcemp@gmail.com>
2022-09-28 21:20:05 -0500

    Assertions and Preconditions

--- 05_errors.playground/Contents.swift
+++ 05_errors.playground/Contents.swift
@@ -28,3 +28,19 @@ do {
 
 
 
+// Asserciones (debug) y precondiciones (build)
+
+let age = -4
+precondition(age >= 0, "La edad de una persona no puede ser negativa")
+//assert(age >= 0, "La edad de una persona no puede ser negativa")
+
+if age > 10 {
+    print("Puedes subir a la montaña rusa")
+}else if age >= 0 {
+    print("Eres demasiado pequeño para subir a la montaña rusa")
+}else {
+    assertionFailure("La edad de una persona no puede ser negativa")
+}
+
+
+
