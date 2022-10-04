cb233de949d2bf03f031e34bc0ea0ef54d5ceb0a
Jaime A Perez R <catecppcemp@gmail.com>
2022-10-04 10:21:05 -0500

    Imput parameters

--- 10_Funtions.playground/Contents.swift
+++ 10_Funtions.playground/Contents.swift
@@ -9,3 +9,32 @@ func greeting(person: String) -> String {
 greeting(person: "Ricardo")
 greeting(person: "Edgar")
 greeting(person: "Juan")
+
+
+func sayHello() -> String {
+    return "Hello"
+}
+sayHello()
+
+
+
+func greeting(person: String, isMale:Bool, age:Int) -> String {
+    switch age {
+    case 1...20:
+        if isMale {
+            return "Bienvenido Señorito \(person)"
+        }else {
+            return "Bienvenida Señorita \(person)"
+        }
+    case  20...110:
+        if isMale {
+            return "Bienvenido Caballero \(person)"
+        }else {
+            return "Bienvenida Señora \(person)"
+        }
+    default:
+        return ""
+    }
+}
+greeting(person: "Ricardo", isMale: true, age: 18)
+greeting(person: "Ana", isMale: false, age: 21)
