89946a87be234b8396e403c493be669b6af25767
Jaime A Perez R <catecppcemp@gmail.com>
2022-10-04 10:36:01 -0500

    Output parameters

--- 10_Funtions.playground/Contents.swift
+++ 10_Funtions.playground/Contents.swift
@@ -17,6 +17,7 @@ func sayHello() -> String {
 sayHello()
 
 
+// Input parameters
 
 func greeting(person: String, isMale:Bool, age:Int) -> String {
     switch age {
@@ -38,3 +39,44 @@ func greeting(person: String, isMale:Bool, age:Int) -> String {
 }
 greeting(person: "Ricardo", isMale: true, age: 18)
 greeting(person: "Ana", isMale: false, age: 21)
+
+
+
+// Output parameters
+
+func greet2(person: String) {
+    print("Hola\(person)")
+}
+greet2(person: "Edgar")
+
+
+func printAndCount(string:String) -> Int{
+    print(string)
+    return string.count
+}
+printAndCount(string: "hola que tal")
+
+func printWithoutCounting(string:String){
+    let _ = printAndCount(string: string)
+}
+printWithoutCounting(string: "hola que tal")
+
+
+func minMax(array: [Int]) -> (min:Int, max:Int)?{
+    
+    if array.isEmpty{return nil}
+    var currentMIn = array[0]
+    var currentMax = array[0]
+    
+    for value in array[1..<array.count]{
+        if value < currentMIn{
+            currentMIn = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    return (currentMIn, currentMax)
+}
+
+let bound = minMax(array: [23,4,4,65,67])
+print("los valores se encuentran entre \(bound!.min) y \(bound!.max)")
