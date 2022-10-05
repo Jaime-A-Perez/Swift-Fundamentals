276147c8d2fcbfcb2ff9cc947f9a841737a8930f
Jaime A Perez R <catecppcemp@gmail.com>
2022-10-05 15:19:29 -0500

    Trailing Closures

--- 11_Closures.playground/Contents.swift
+++ 11_Closures.playground/Contents.swift
@@ -23,3 +23,60 @@ reversedNames = names.sorted(by: { (s1, s2) in return s1 > s2})
 reversedNames = names.sorted(by: { (s1, s2) in s1 > s2})
 reversedNames = names.sorted(by: { $0 > $1 })
 reversedNames = names.sorted(by: > )
+
+
+
+
+// Trailing Closure
+
+func someFunctionThatTakeAClosure(closure: () -> Void) {
+//    Body of function
+}
+
+someFunctionThatTakeAClosure(closure: {
+//    Body of Closure
+})
+
+someFunctionThatTakeAClosure {
+//    Body of Closure
+}
+
+
+reversedNames = names.sorted{$0 < $1}
+
+
+
+let digitNames = [0: "Cero", 1: "Uno", 2: "Dos", 3: "Tres", 4: "Cuatro",
+                  5: "Cinco", 6: "Seis", 7: "Siete", 8: "Ocho", 9: "Nueve" ]
+
+let numbers = [12, 58, 510, 2127]
+
+let numberStrings = numbers.map { (number) -> String in
+    var number = number
+    var output = ""
+    repeat{
+        output = digitNames[number % 10]! + output  // All cases are contempled - Justify force and rapping
+        number /= 10
+    }while number > 0
+    return output
+}
+
+let realNumbers = [-12, 58, 510, -2127]
+
+let numbersRealStrings = realNumbers.map { (number) -> String in
+    var number = number
+    var output = ""
+    if number < 0 {
+        repeat{
+            output = digitNames[-number % 10 ]! + output
+            number /= 10
+        }while number > 0 ? number > 0 : number < 0
+        output = "Menos\(output)"
+    }else {
+        repeat{
+            output = digitNames[number % 10]! + output
+            number /= 10
+        }while number > 0
+    }
+    return output
+}
